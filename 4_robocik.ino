f7aeda21ea16a9621bb8ad59deec18cdc9884fa6
Patryk Skibniewski <56847676+RyKaT07@users.noreply.github.com>
2025-12-12 15:20:13 +0100

    Add total distance counters and update travel function
    Added total distance counters and modified interrupt handlers to increment these counters. Updated the distance travel function to utilize the new counters.

--- 4_robocik.ino
+++ 4_robocik.ino
@@ -270,3 +270,104 @@ void loop() {
     czasOstatniegoWyslania = teraz;
   }
 }
+
+
+
+// ... (istniejące zmienne)
+
+// DODAJ TO: Liczniki całkowite drogi
+volatile unsigned long licznikTotal_L = 0;
+volatile unsigned long licznikTotal_P = 0;
+
+// PARAMETR DO KALIBRACJI:
+// Ile impulsów enkodera przypada na 1 pełny obrót KOŁA wyjściowego?
+// Szacunek na podstawie Twojego 'wspolczynnika': ~58.33 impulsu na obrót koła.
+// Jeśli robot przejedzie 1m, a pokaże 0.8m -> zwiększ tę liczbę.
+const float IMPULSY_NA_OBROT_KOLA = 58.33; 
+
+// ...
+
+// ZMODYFIKOWANE PRZERWANIA
+void przerwanieL() {
+  unsigned long t = millis();
+  pomiary_L[numer_L] = int(t - pomiar_L);
+  pomiar_L = t;
+  numer_L++;
+  if (numer_L > 3) numer_L = 0;
+  czasBrakuImpulsu_L = t;
+  
+  licznikTotal_L++; // <--- DODANO
+}
+
+void przerwanieP() {
+  unsigned long t = millis();
+  pomiary_P[numer_P] = int(t - pomiar_P);
+  pomiar_P = t;
+  numer_P++;
+  if (numer_P > 3) numer_P = 0;
+  czasBrakuImpulsu_P = t;
+  
+  licznikTotal_P++; // <--- DODANO
+}
+
+// Funkcja realizująca jazdę na zadaną odległość [m]
+void jedzDystans(float metry, float predkosc_ms) {
+  // 1. Oblicz ile to impulsów
+  float obwod = PI * srednicaKola; // w metrach
+  float wymaganeObroty = metry / obwod;
+  unsigned long wymaganeImpulsy = (unsigned long)(wymaganeObroty * IMPULSY_NA_OBROT_KOLA);
+
+  // 2. Zapamiętaj stan początkowy liczników
+  unsigned long startL = licznikTotal_L;
+  unsigned long startP = licznikTotal_P;
+
+  // 3. Ruszamy (zakładamy jazdę prosto, czyli R = bardzo dużo)
+  ustawJazdePoLuku(predkosc_ms, 1000.0);
+
+  Serial.print("Jade dystans: "); Serial.print(metry); 
+  Serial.print("m. Cel impulsow: "); Serial.println(wymaganeImpulsy);
+
+  // 4. Pętla oczekiwania - jedź dopóki średnia z obu kół nie osiągnie celu
+  while (true) {
+    // Oblicz ile przejechały
+    unsigned long przebyteL = licznikTotal_L - startL;
+    unsigned long przebyteP = licznikTotal_P - startP;
+    unsigned long srednia = (przebyteL + przebyteP) / 2;
+
+    // Sprawdź czy dojechaliśmy
+    if (srednia >= wymaganeImpulsy) {
+      break; // Wyjście z pętli
+    }
+
+    // WAŻNE: W pętli musimy aktualizować PID i obsługiwać bezpieczeństwo!
+    obslugaBrakuImpulsu();
+    aktualizujPID();
+    
+    // Opcjonalnie: wizualizacja postępu co 100ms
+    static unsigned long lastPrint = 0;
+    if (millis() - lastPrint > 100) {
+      Serial.print("Postep: "); Serial.print(srednia);
+      Serial.print("/"); Serial.println(wymaganeImpulsy);
+      lastPrint = millis();
+    }
+  }
+
+  // 5. Stop po dojechaniu
+  predkoscZadana_L = 0;
+  predkoscZadana_P = 0;
+  pwmWyjscie_L = 0;
+  pwmWyjscie_P = 0;
+  analogWrite(pinSilnikL, 0);
+  analogWrite(pinSilnikP, 0);
+  Serial.println("Dystans osiagniety. STOP.");
+}
+
+// wewnątrz switch(cCo) w funkcji obslugaKomunikacji:
+
+    case 'x': // Komenda: x0.5 -> jedź 0.5 metra z domyślną prędkością
+      {
+        float dystans = Serial.parseFloat();
+        // Domyślna prędkość np. 0.2 m/s, można zmienić
+        jedzDystans(dystans, 0.2); 
+      }
+      break;
